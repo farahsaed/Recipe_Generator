97cdf75d934aea9fdf2c75f9839082f7c546f6ed
NourEsmat <118803964+NourEsmat@users.noreply.github.com>
2024-02-02 01:06:51 +0200

    notification for comments

--- Recipe_Generator/Controllers/CommentController.cs
+++ Recipe_Generator/Controllers/CommentController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Recipe_Generator.Data;
 using Recipe_Generator.DTO;
+using Recipe_Generator.Interface;
 using Recipe_Generator.Models;
 
 namespace Recipe_Generator.Controllers
@@ -13,19 +15,21 @@ namespace Recipe_Generator.Controllers
     {
         private readonly RecipeContext _db;
         private readonly UserManager<User> _userManager;
+        private readonly IEmailSender emailSender;
 
-        public CommentController(RecipeContext db,UserManager<User> userManager)
+        public CommentController(RecipeContext db,UserManager<User> userManager,IEmailSender emailSender)
         {
             this._userManager = userManager;
+            this.emailSender = emailSender;
             this._db = db;
         }
 
         [HttpPost("AddComment/{rid}")]
-        public IActionResult CreateComment(CommentsDTO commentDTO,int rid)
+        public async Task<IActionResult> CreateComment(CommentsDTO commentDTO,int rid)
         {
             var userId = _userManager.GetUserId(HttpContext.User);
-            var recipe = _db.Recipes.SingleOrDefault(c => c.Id == rid);
-
+            var recipe = _db.Recipes.Include(c => c.User).SingleOrDefault(c => c.Id == rid);
+            var user = await _userManager.FindByIdAsync(userId);
             if(userId == null)
             {
                 return NotFound("Unauthorized user");
@@ -44,6 +48,9 @@ namespace Recipe_Generator.Controllers
             comment.RecipeId = recipe.Id;
             _db.Comments.Add(comment);
             _db.SaveChanges();
+            await emailSender.SendEmailNotification(recipe.User.Email, 
+                recipe.User.FirstName + " " + recipe.User.LastName,
+                comment.Description,comment.CreatedOn,user.UserName,"comment");
             return Ok(comment);
         }
 
